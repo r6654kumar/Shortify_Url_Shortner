17ed945d2ac9c9463dcb0d8aeaa1271e65dd47e2
Rahul <rsc120@nist.edu>
2024-11-19 17:49:03 +0530

    Project Initialized

---
+++ src/main/java/com/shortify/Shortify_Url_Shortner/ShortifyUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.shortify.Shortify_Url_Shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortifyUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortifyUrlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shortify/Shortify_Url_Shortner/ShortifyUrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortify.Shortify_Url_Shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortifyUrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
