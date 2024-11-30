d75dc4d84d7487acb93b629dbb02ca6e9cbad71a
Rahul <rsc120@nist.edu>
2024-11-30 19:49:55 +0530

    Controller Updated

--- src/main/java/com/shortify/Shortify_Url_Shortner/controller/UrlShortenerController.java
+++ src/main/java/com/shortify/Shortify_Url_Shortner/controller/UrlShortenerController.java
@@ -1,11 +1,16 @@
 package com.shortify.Shortify_Url_Shortner.controller;
 
+import com.shortify.Shortify_Url_Shortner.exceptions.UrlNotFoundException;
 import com.shortify.Shortify_Url_Shortner.model.ShortenedURL;
 import com.shortify.Shortify_Url_Shortner.repository.ShortenedUrlRepository;
 import com.shortify.Shortify_Url_Shortner.service.UrlShortenerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/url")
@@ -22,5 +27,16 @@ public class UrlShortenerController {
 //        System.out.println(originalURL);
         ShortenedURL shortURL = urlShortenerService.shortenURL(originalURL);
         return ResponseEntity.ok(shortURL.toString());
+    }
+    @GetMapping("/redirect")
+    public ResponseEntity<Void> redirectToOriginalURL(@RequestParam("shortURL")String shortURL){
+        try{
+            String originalURL= urlShortenerService.getOriginalURL(shortURL);
+            return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(originalURL)).build();
+        }catch(UrlNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
